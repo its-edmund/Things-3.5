28e4f8d881537e95360bfa8082030825fc3a04f5
Edmund Xin <mxinburritos@gmail.com>
2024-05-17 12:09:53 -0700

    Initial Commit

---
+++ Things 3.5/Things_3_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Things_3_5App.swift
+//  Things 3.5
+//
+//  Created by ByteDance on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Things_3_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
